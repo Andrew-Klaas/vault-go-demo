0e86ef411c7b423f14beeb255cf768b455fbbf09
Klaas <klaas@Andrews-MacBook-Air.local>
2023-06-14 11:50:51 -0500

    update

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
 	"github.com/Andrew-Klaas/vault-go-demo/users"
 )
 
+//Testing
+
 func main() {
 	http.Handle("/public/", http.StripPrefix("/public", http.FileServer(http.Dir("public"))))
 
